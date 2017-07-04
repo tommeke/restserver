69b40cf472f712221dd9db835707597bc1040f55
tommeke <tomderay@gmail.com>
2017-07-04 17:54:38 +0200

    initial commit

---
+++ src/main/java/com/broochem/restserver/App.java
@@ -0,0 +1,32 @@
+package com.broochem.restserver;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		ResourceConfig config = new ResourceConfig();
+		config.packages("com.broochem.restserver");
+		ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+		Server server = new Server(8888);
+		ServletContextHandler context = new ServletContextHandler(server, "/*");
+		context.addServlet(servlet, "/*");
+
+		try {
+			server.start();
+			server.join();
+		} finally {
+			server.destroy();
+		}
+
+	}
+}
---
+++ src/main/java/com/broochem/restserver/Resource.java
@@ -0,0 +1,43 @@
+package com.broochem.restserver;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("home")
+public class Resource {
+	@GET
+	@Path("hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		return "Hello, world!";
+	}
+	
+	@GET
+	@Path("param")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String paramMethod(@QueryParam("name") String name) {
+	  return "Hello, " + name;
+	}
+
+	@GET
+	@Path("path/{var}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String pathMethod(@PathParam("var") String name) {
+	    return "Hello, " + name;
+	}
+	
+	@POST
+	@Path("post")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public String postMethod(@FormParam("name") String name) {
+	  return "<h2>Hello, " + name + "</h2>";
+	}
+}
