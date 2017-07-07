0108b5d400281b416a91973fef7aa4268918dccb
tommeke <tomderay@gmail.com>
2017-07-07 12:07:02 +0200

    suddenly both static and dynamic content work...strange...

--- src/main/java/com/broochem/restserver/App.java
+++ src/main/java/com/broochem/restserver/App.java
@@ -1,14 +1,10 @@
 package com.broochem.restserver;
 
-import java.io.File;
-
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -30,39 +26,24 @@ public class App {
 		restContext.addServlet( servlet, "/*");
 		
 		// Jetty static content
-		/*
 		ResourceHandler staticResourceHandler = new ResourceHandler();
 	    staticResourceHandler.setResourceBase("./src/main/resource/webapp/");
-	    
-	    System.out.println( (new File(".")).getCanonicalPath() );
-
 	    ContextHandler staticContextHandler = new ContextHandler("/");
 	    staticContextHandler.setHandler( staticResourceHandler);
-		*/
-		
-		   ServletContextHandler context = new ServletContextHandler();
-		    context.setContextPath("/");
-
-		    ServletHolder staticHolder = new ServletHolder(new DefaultServlet());
-		    staticHolder.setInitParameter("resourceBase", "./src/main/resource/webapp/");
-		    context.addServlet(staticHolder, "/");
-
-		
-		
+				
 	    // add handlers
 		Server server = new Server(8888);
 
 		HandlerList handlers = new HandlerList();
 		handlers.setHandlers( new Handler[] {
 				restContext,
-				context
+				staticContextHandler
 		});
 		
 		server.setHandler( handlers);
 		
 		try {
 			server.start();
-			server.dump( System.err);
 			server.join();
 		} finally {
 			server.destroy();
