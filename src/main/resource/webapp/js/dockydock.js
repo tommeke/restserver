5f18646183f1e6d5de412bf9be5412debae4a306
tommeke <tomderay@gmail.com>
2017-07-15 01:12:06 +0200

    do the menu with drag & drop panels.

--- src/main/resource/webapp/js/dockydock.js
+++ src/main/resource/webapp/js/dockydock.js
@@ -2,40 +2,75 @@
 
 The custom docky dock js code talking to the REST server
 
+var obj = JSON.parse (jsonString);
+
+var str = JSON.stringify (obj);
+
 */
 
 (function($){
 
+/*
+  // header layout generated and fetched from the rest server
+  var headerDOM = global.document.getElementById("header-content");
+  global.$ajaxUtils.sendGetRequest( "/layout/list",
+      function(res) {
+        var layoutList = JSON.parse (res);
+        // generate header buttons
+        for ( var i = 0; i < layoutList.length; i++) {
+          var button = global.document.createElement( 'button');
+          var typeAttr = global.document.createAttribute ( 'type');
+          typeAttr.value = 'button';
+          var classAttr = global.document.createAttribute( 'class');
+          classAttr.value = 'btn btn-primary';
+          button.setAttributeNode( typeAttr);
+          button.setAttributeNode( classAttr);
+          button.appendChild( global.document.createTextNode( layoutList[i].name));
+          headerDOM.appendChild( button);
+        }
+      }
+  );
+*/
+  // docking layout
   var config = {
       content: [{
           type: 'row',
-          content:[{
-              type: 'component',
-              componentName: 'testComponent',
-              componentState: { label: 'WHAT IS GOING ON HERE' }
-          },{
-              type: 'column',
-              content:[{
-                  type: 'component',
-                  componentName: 'testComponent',
-                  componentState: { label: 'B' }
-              },{
-                  type: 'component',
-                  componentName: 'testComponent',
-                  componentState: { label: 'C' }
-              }]
+          content: [{
+              type:'component',
+              componentName: 'example',
+              componentState: { text: 'Component 1' }
+          },
+          {
+              type:'component',
+              componentName: 'example',
+              componentState: { text: 'Component 2' }
           }]
       }]
   };
 
-  var myLayout = new GoldenLayout( config, document.getElementById("main-content"));
+  var myLayout = new window.GoldenLayout( config, $('#layoutContainer') );
 
-  myLayout.registerComponent( 'testComponent', function( container, componentState ){
-      container.getElement().html( '<h2>' + componentState.label + '</h2>' );
+  myLayout.registerComponent( 'example', function( container, state ){
+      container.getElement().html( '<h2>' + state.text + '</h2>');
   });
 
-  myLayout.init();
+    myLayout.init();
 
+  var addMenuItem = function( text ) {
+      var element = $( '<li>' + text + '</li>' );
+      $( '#menuContainer' ).append( element );
 
+      //insertion code will go here
+      var newItemConfig = {
+          type: 'component',
+          componentName: 'example',
+          componentState: { text: text }
+      };
+
+      myLayout.createDragSource( element, newItemConfig );
+  };
+
+  addMenuItem( 'User added component A' );
+  addMenuItem( 'User added component B' );
 
 })(window.$);
